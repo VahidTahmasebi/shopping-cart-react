5d0548b28d182ac27eefcffb09cca7a861a85f1d
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-01-24 15:45:28 +0330

    Add HomePage file

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,13 @@
+import Layout from "../Layout/Layout";
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <main className='container'>
+        <section className='productList'>this is HomePage</section>
+      </main>
+    </Layout>
+  );
+};
+
+export default HomePage;
