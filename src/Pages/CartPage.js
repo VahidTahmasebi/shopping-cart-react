e55e743ccb210a387b81094c5b4e8f593c0dd699
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-01-24 20:16:25 +0330

    Add product box structure in cartPage file

--- src/Pages/CartPage.js
+++ src/Pages/CartPage.js
@@ -1,12 +1,30 @@
 import Layout from "../Layout/Layout";
+import { useCart, useCartActions } from "../Providers/CartProvider";
 
 const CartPage = () => {
+  const { cart, total } = useCart();
+  const dispatch = useCartActions();
   return (
     <Layout>
       <main className='container'>
         <section className='cartCenter'>
           <section className='cardItemList'>
-            this is CartPage
+            {cart.map((item) => {
+              return (
+                <div className='cardItem' key={item.id}>
+                  <div className='itemImg'>
+                    <img src={item.image} alt={item.name} />
+                  </div>
+                  <div>{item.name}</div>
+                  <div>{item.offPrice * item.quantity}</div>
+                  <div className='btnGroup'>
+                    <button>-</button>
+                    <p>{item.quantity}</p>
+                    <button>+</button>
+                  </div>
+                </div>
+              );
+            })}
           </section>
         </section>
       </main>
