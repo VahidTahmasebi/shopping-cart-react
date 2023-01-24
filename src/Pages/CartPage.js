fd2ee606ab6474722bcdb5669d084ac82da71db0
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-01-24 17:00:31 +0330

    Add cartpage file

---
+++ src/Pages/CartPage.js
@@ -0,0 +1,17 @@
+import Layout from "../Layout/Layout";
+
+const CartPage = () => {
+  return (
+    <Layout>
+      <main className='container'>
+        <section className='cartCenter'>
+          <section className='cardItemList'>
+            this is CartPage
+          </section>
+        </section>
+      </main>
+    </Layout>
+  );
+};
+
+export default CartPage;
