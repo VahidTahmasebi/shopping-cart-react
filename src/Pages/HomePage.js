a7f872ff0a7054d9a23dbf7d57eb7a8af097ac99
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-01-24 16:44:49 +0330

    Add products section

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,34 @@
 import Layout from "../Layout/Layout";
+import * as data from "../server/data";
+
+const addProductHandler = (product) => {
+  console.log(product);
+};
 
 const HomePage = () => {
   return (
     <Layout>
       <main className='container'>
-        <section className='productList'>this is HomePage</section>
+        <section className='productList'>
+          {data.products.map((product) => {
+            return (
+              <section className='product'>
+                <div className='productImg'>
+                  <img src={product.image} alt={product.name} />
+                </div>
+                <div className='productDesc'>
+                  <p>{product.name}</p>
+                  <p> $ {product.price}</p>
+                  <button
+                    onClick={() => addProductHandler(product)}
+                    className='btn primary'>
+                    Add to cart
+                  </button>
+                </div>
+              </section>
+            );
+          })}
+        </section>
       </main>
     </Layout>
   );
