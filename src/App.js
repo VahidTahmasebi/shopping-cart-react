2370ada8d4a70469bc9e653aa204996b190787d6
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-01-25 16:46:04 +0330

    Add auth context in authProvider file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import CartPage from "./Pages/CartPage";
+import CheckoutPage from "./Pages/CheckoutPage";
 import HomePage from "./Pages/HomePage";
 import LoginPage from "./Pages/LoginPage";
 import ProfilePage from "./Pages/ProfilePage";
 import SignupPage from "./Pages/SignupPage";
+import AuthProvider from "./Providers/AuthProvider";
 import CartProvider from "./Providers/CartProvider";
 
 function App() {
   return (
     <BrowserRouter>
-      <CartProvider>
-        <div className='App'>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/cart' element={<CartPage />} />
-            <Route path='/signup' element={<SignupPage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/profile' element={<ProfilePage />} />
-          </Routes>
-        </div>
-      </CartProvider>
+      <AuthProvider>
+        <CartProvider>
+          <div className='App'>
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/signup' element={<SignupPage />} />
+              <Route path='/login' element={<LoginPage />} />
+              <Route path='/profile' element={<ProfilePage />} />
+              <Route path='/cart' element={<CartPage />} />
+            </Routes>
+          </div>
+        </CartProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
