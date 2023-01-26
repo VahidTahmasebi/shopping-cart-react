faa59c2862433782fbc78b891ab1104188a6f716
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-01-26 17:37:05 +0330

    Upgrade checkout page

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,7 +1,48 @@
+import { Link } from "react-router-dom";
+import { useAuth } from "../../Providers/AuthProvider";
+import { useCart } from "../../Providers/CartProvider";
+
 const Checkout = () => {
-    return ( 
-        <div>this is checkout</div>
-     );
-}
- 
+  const auth = useAuth();
+  const { cart, total } = useCart();
+
+  if (!cart.length)
+    return (
+      <main className='container'>
+        <Link to='/'>go to shopping</Link>
+      </main>
+    );
+
+  return (
+    <main className='container'>
+      <section className='cartCenter'>
+        {auth ? (
+          <>
+            <section className='cartCast'>
+              <h3>checkout detail</h3>
+              <p>name: {auth.name}</p>
+              <p>email: {auth.email}</p>
+              <p>phone number: {auth.phoneNumber}</p>
+            </section>
+            <section className='cartCast'>
+              {cart &&
+                cart.map((c) => {
+                  return (
+                    <div>
+                      {c.name} * {c.quantity} : {c.quantity * c.offPrice}
+                    </div>
+                  );
+                })}
+              <hr />
+              <div>total price : {total}</div>
+            </section>
+          </>
+        ) : (
+          <p>please login !</p>
+        )}
+      </section>
+    </main>
+  );
+};
+
 export default Checkout;
